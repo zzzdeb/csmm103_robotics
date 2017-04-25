be89d5a6251836a1acfbc8cbfcb0531754e6bd69
Zolboo Erdenebayar <zolboo.erdenebayar@rwth-aachen.de>
2017-04-25 17:38:33 +0200

    pj1

---
+++ pj1/project1_solution/scripts/solution.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python  
+import rospy
+
+from std_msgs.msg import Int16
+from project1_solution.msg import TwoInts
+
+def listener():
+  	rospy.init_node('two_int_listener',anonymous-True)
+ 	sub = rospy.Subscriber('two_ints', TwoInts,callback)
+   
+   	rospy.spin()
+
+def callback(msg):
+ 	rospy.log_info(rospy.get_caller_id()+'I heard %s', msg.data)
+ 	
+
+
+if __name__="__main__":
+ 	try:
+   		listener()
+    except rospy.ROSInterruptException:
+     	pass
